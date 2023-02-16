d5157e0f21c32beb5d2dc6205cd578c1ed5a36b3
Kram9453 <92831630+Kram9453@users.noreply.github.com>
2023-02-16 12:12:01 -0500

    Add files via upload

---
+++ lab1---pt2.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : lab1---pt2.cpp
+// Author      : Krishna Ramlall
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int times;
+cout << "Enter an integer: \n";
+cin >> times;
+	for (int i = 0;i<times;i++)
+	{
+		cout << "Hello World!\n";
+	}
+	return 0;
+}
---
+++ lab1-pt1.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : lab1-pt1.cpp
+// Author      : Krishna Ramlall
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+	for (int i = 0;i<5;i++)
+	{
+		cout << "Hello World!\n";
+	}
+	return 0;
+}
