104befea9e2db2b936508dddc69a130e0df52399
Kram9453 <92831630+Kram9453@users.noreply.github.com>
2023-02-16 12:13:32 -0500

    Add files via upload

---
+++ lab1---pt2.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : lab1---pt2.cpp
+// Author      : Krishna Ramlall
+// Version     :
+// Copyright   : 
+// Description : This is lab 1 Part 2 for EMT 2410!
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int times;
+cout << "Enter an integer: \n";
+cin >> times;
+	for (int i = 0;i<times;i++)
+	{
+		cout << "Hello World!\n";
+	}
+	return 0;
+}
---
+++ lab1-pt1.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : lab1-pt1.cpp
+// Author      : Krishna Ramlall
+// Version     :
+// Copyright   : 
+// Description : Lab 1 Part 1 for EMT 2410!
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+	for (int i = 0;i<5;i++)
+	{
+		cout << "Hello World!\n";
+	}
+	return 0;
+}
